f38c69310e56654695f6904f41349083e95a1de8
ozatarigan <btarigan@uwaterloo.ca>
2023-06-07 22:09:36 -0400

    Adding FLOAT data type to db

--- habit_tracker.py
+++ habit_tracker.py
@@ -18,14 +18,14 @@ cur.execute('''
         date TEXT
         , user_name TEXT
         , user_mood TEXT
-        , training_time INTEGER
-        , learning_time INTEGER
-        , success INTEGER
+        , training_time FLOAT
+        , learning_time FLOAT
+        , success FLOAT
     )
 '''
 )
 
-# Setting up user input
+# Setting up user input - Defining Function
 
 user_name = 'OT'
 user_mood = input(f"How do you feel physically, emotionally, and physically today {user_name}? ")
@@ -33,26 +33,25 @@ training_time = float(input("Enter the amount of time trained: "))
 run_distance = float(input("Enter the amount you ran today: "))
 learning_time = float(input('Enter the amount of time you learned something today: '))
 
-# Goal definition & success counter
 
-train_learn_goal = 0.5
-run_goal = 0.1
-success_counter = 0
-fail_counter = 0
+def day_summary():
+    train_learn_goal = 0.5
+    run_goal = 0.1
+    success_counter = 0
+    fail_counter = 0
+    date = datetime.now()
+    
+    print(f'Hi {user_name}, here is a summary of your habits today {date}')
+    print(f'You trained {training_time} minutes, ran {run_distance} km and learned for {learning_time} minutes.')
 
-# Summary console
+    if learning_time and training_time >= train_learn_goal and run_distance >= run_goal:
+        print("Congratulations, you've achieved your goal today")
+        success_counter += 1
+    else:
+        print("You'll do better, but you did not meet your goals today")
+        fail_counter += 1
 
-date = datetime.now()
-print(f'Hi {user_name}, here is a summary of your habits today {date}')
-print(f'You trained {training_time} minutes, ran {run_distance} km and learned for {learning_time} minutes.')
-
-if learning_time and training_time >= train_learn_goal and run_distance >= run_goal:
-    print("Congratulations, you've achieved your goal today")
-    success_counter += 1
-else:
-    print("You'll do better, but you did not meet your goals today")
-    fail_counter += 1
-
+day_summary()
 # Closing connection
 
 con.close()
